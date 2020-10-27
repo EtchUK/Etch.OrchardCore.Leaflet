1567fa9375f3891a1ee1dcc7d49d69a6d93762a1
Peter Keating <pete.keating@etchuk.com>
2020-10-27 22:23:58 +0000

    Remove return type on function as always returned the same value

--- Etch.OrchardCore.Leaflet/Assets/Admin/js/tiles/storeImageDimensions.ts
+++ Etch.OrchardCore.Leaflet/Assets/Admin/js/tiles/storeImageDimensions.ts
@@ -1,4 +1,4 @@
-const storeImageDimensions = (e: Event): boolean => {
+const storeImageDimensions = (e: Event): void => {
     e.preventDefault();
 
     const draftBtn = document.querySelector('button[value="submit.Publish"]') as HTMLButtonElement;
@@ -7,25 +7,18 @@ const storeImageDimensions = (e: Event): boolean => {
     draftBtn.disabled = true;
     submitBtn.disabled = true;
 
-    try {
-        const src = (document.querySelector('#mediaContainerMain .thumb-container img') as HTMLImageElement).src.split('?')[0]
-        const img = new Image();
+    const src = (document.querySelector('#mediaContainerMain .thumb-container img') as HTMLImageElement).src.split('?')[0]
+    const img = new Image();
 
-        img.onload = () => {
-            (document.querySelector('#MapTiles_Height') as HTMLInputElement).value = img.height.toString();
-            (document.querySelector('#MapTiles_Width') as HTMLInputElement).value = img.width.toString();
+    img.onload = () => {
+        (document.querySelector('#MapTiles_Height') as HTMLInputElement).value = img.height.toString();
+        (document.querySelector('#MapTiles_Width') as HTMLInputElement).value = img.width.toString();
 
-            draftBtn.disabled = false;
-            submitBtn.disabled = false;
-        }
-
-        img.src = src;
-    }
-    catch {
-        return false;
+        draftBtn.disabled = false;
+        submitBtn.disabled = false;
     }
 
-    return false;
+    img.src = src;
 };
 
 export default storeImageDimensions;

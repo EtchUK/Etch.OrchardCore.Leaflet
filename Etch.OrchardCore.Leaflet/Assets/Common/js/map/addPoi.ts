ba4617d49826ac9c7781479fcddd87594acac2f4
Peter Keating <pete.keating@etchuk.com>
2020-10-27 15:03:16 +0000

    Set marker icon to center position

--- Etch.OrchardCore.Leaflet/Assets/Common/js/map/addPoi.ts
+++ Etch.OrchardCore.Leaflet/Assets/Common/js/map/addPoi.ts
@@ -7,7 +7,7 @@ const addPoi = (map: L.Map, poi: IPoi): IMapMarker => {
     const icon = L.icon({
         iconUrl: poi.icon.path,
 
-        iconAnchor: [poi.icon.width / 2, poi.icon.height],
+        iconAnchor: [poi.icon.width / 2, poi.icon.height / 2],
         iconSize: [poi.icon.width, poi.icon.height],
     });
 

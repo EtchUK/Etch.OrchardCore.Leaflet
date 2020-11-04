1ce5bf9627a8dd79a975e1278a33223f7ab5e83f
Peter Keating <pete.keating@etchuk.com>
2020-11-04 14:57:26 +0000

    v0.4.0

--- Etch.OrchardCore.Leaflet/Manifest.cs
+++ Etch.OrchardCore.Leaflet/Manifest.cs
@@ -6,6 +6,6 @@ using OrchardCore.Modules.Manifest;
     Dependencies = new string[] { "OrchardCore.BackgroundTasks", "OrchardCore.Media", "OrchardCore.Title" },
     Description = "Create interactive maps utilising Leaflet.",
     Name = "Leaflet Maps",
-    Version = "0.3.1",
+    Version = "0.4.0",
     Website = "https://etchuk.com"
 )]

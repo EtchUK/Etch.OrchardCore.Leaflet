25515b4d505afa8f307dfec50438ee85d79291e3
Peter Keating <pete.keating@etchuk.com>
2020-11-17 16:25:33 +0000

    Fix parse error when POI content contains apostrophe

---
+++ Etch.OrchardCore.Leaflet/Utils/PoiUtils.cs
@@ -0,0 +1,18 @@
+﻿using Etch.OrchardCore.Leaflet.Dtos;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+
+namespace Etch.OrchardCore.Leaflet.Utils
+{
+    public static class PoiUtils
+    {
+        public static string Serialize(IEnumerable<PoiMarker> markers)
+        {
+            return JsonConvert.SerializeObject(markers, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            }).Replace("'", @"\'");
+        }
+    }
+}
